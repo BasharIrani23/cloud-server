57aa279343d0b509f9b66690cde3bb71a3c88764
Basharirani23 <super_boshbosh@hotmail.com>
2023-07-24 12:22:15 +0300

    Deploy

--- files/server.js
+++ files/server.js
@@ -1,22 +1,20 @@
-'use strict';
-
-const express = require('express');
+"use strict";
+require("dotenv").config();
+const express = require("express");
 const app = express();
 app.use(express.json());
 
+app.get("/", handelHomePage);
+app.get("/api", handelApiPage);
 
-app.get('/', handelHomePage);
-app.get('/api', handelApiPage);
-
-
-function handelHomePage(req,res){
-    res.status(200).json('Welcome To Home Page')
+function handelHomePage(req, res) {
+    res.status(200).json("Welcome To Home Page");
 }
 
-
-function handelApiPage(req,res){
-    res.status(200).json('Welcome to Api Page' )
+function handelApiPage(req, res) {
+    res.status(200).json("Welcome to Api Page");
 }
-
- app.listen(3000, ()=> {console.log('Up and running on port: 3000')
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Up and running on port: 3000");
+});
